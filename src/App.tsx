53a1cebe8bd22529c52c085b47924166e3d72c7b
unknown <olaoluwaojedapo@gmail.com>
2021-09-14 10:01:32 +0100

    feat: first commit (Creadted the compnent file needed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Balance from './component/Balance'
+import Card from './component/Card'
+import Days from './component/Days'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="flex">
+          <Balance />
+          <Days />
+        </div>
+        <div className="flex">
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/Days.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Days() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Days;
